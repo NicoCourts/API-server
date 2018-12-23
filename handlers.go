b0e3fbaf9791a282898bf93727942d7ef3803c7e
nicocourts <nico@nicocourts.com>
2018-12-23 13:20:37 -0800

    ffff

--- handlers.go
+++ handlers.go
@@ -52,7 +52,10 @@ func PostShow(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 
-	if err := json.NewEncoder(w).Encode(RepoGetPost(postID)); err != nil {
+	p := RepoGetPost(postID)
+	log.Printf(string(p.ID))
+
+	if err := json.NewEncoder(w).Encode(p); err != nil {
 		panic("Error with JSON encoding")
 	}
 }
