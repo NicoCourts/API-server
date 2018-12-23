8380ea8a237d7c7a000cb3e234e8b64ca1942749
nicocourts <nico@nicocourts.com>
2018-12-23 11:17:08 -0800

    Add logging

--- logger.go
+++ logger.go
@@ -6,8 +6,6 @@ import (
 	"time"
 )
 
-const logfile = "/var/log/api_server.log"
-
 // Logger wraps a web handler to add logging functionality.
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
